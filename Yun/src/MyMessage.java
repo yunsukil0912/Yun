154dbc0f50b467d89ff2c68470487c6846baf387
unknown <aszx0912@naver.com>
2018-06-07 10:03:44 +0900

    new master

--- Yun/src/MyMessage.java
+++ Yun/src/MyMessage.java
@@ -1,7 +1,7 @@
 public class MyMessage {
 	String message;
 	public MyMessage(String message) {
-		this.message = message;
+		this.message ="No message";
 	}
 	public void print() {
 		System.out.println(message);
